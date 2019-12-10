767bc3ab7fa1df28adde30aeb5641d5d785a6f99
Ryan L. Smith (Live Agent) <ryan.smith@salesforce.com>
2019-12-10 15:29:19 -0800

    Part 2

--- 2019/10/2.py
+++ 2019/10/2.py
@@ -1,5 +1,6 @@
 import itertools
 import math
+import numpy as np
 
 def main():
     paths = {}
@@ -10,26 +11,56 @@ def main():
             if _map[h][w] == "#":
                 asteroids.append((w, h))
 
-    for a, b in itertools.combinations(asteroids, 2):
+    #print(asteroids)
+    origin = (5,8)
+    _list = [a for a in asteroids if a != origin]
+    origin = (0,0)
+    #print(_list)
+    #return
+    for point in _list:
+        point = (point[0] - 5, point[1] - 8)
+        #print(str(origin[::1]) + " to " + str(point[::1]))
+
+        slope = math.degrees(math.atan2((point[1] - origin[1]), (point[0] - origin[0])))
+        ang1 = np.arctan2(*origin[::-1])
+        print(ang1)
+        ang2 = np.arctan2(*point[::-1])
+        print(ang2)            
+        print(str(origin[::1]) + " to " + str(point[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
+        dot = point[0] * origin[0] + point[1] * origin[1]
+        det = point[1] * origin[0] - point[0] * origin[1]
+        angle = math.atan2(det, dot)
+        print("Angle: " + str(np.rad2deg(angle % (2 * np.pi))))
+
+        continue
+
         if not a in paths:
             paths[a] = {}
-        try:
-            slope = math.degrees(math.atan2((b[1] - a[1]), (b[0] - a[0])))
-        except ZeroDivisionError:
-            slope = None
-            if a[1] > b[1]:
-                slope = "-None"
+        slope = math.degrees(math.atan2((b[1] - a[1]), (b[0] - a[0])))
         paths[a][slope] = True
+
+        if a == (5,8):
+            ang1 = np.arctan2(*b[::-1])
+            print(ang1)
+            ang2 = np.arctan2(*a[::-1])
+            print(ang2)
+            print(str(b[::1]) + " to " + str(a[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
 
         if not b in paths:
             paths[b] = {}
-        try:
-            slope = math.degrees(math.atan2((a[1] - b[1]), (a[0] - b[0])))
-        except ZeroDivisionError:
-            slope = None
-            if b[1] > a[1]:
-                slope = "-None"
+        slope = math.degrees(math.atan2((a[1] - b[1]), (a[0] - b[0])))
         paths[b][slope] = True
+
+        if b == (5,8):
+            ang1 = np.arctan2(*a[::-1])
+            print(ang1)
+            ang2 = np.arctan2(*b[::-1])
+            print(ang2)            
+            print(str(a[::1]) + " to " + str(b[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
+            dot = b[0] * a[0] + b[1] * a[1]
+            det = b[1] * a[0] - b[0] * a[1]
+            angle = math.atan2(det, dot)
+            print("Angle: " + str(np.rad2deg(angle % (2 * np.pi))))
     
     _path = None
     _max = 0
