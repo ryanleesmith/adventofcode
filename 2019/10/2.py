0875f5d05b738747bb7473ed173f25e84291685c
Ryan L. Smith (Live Agent) <ryan.smith@salesforce.com>
2019-12-11 12:54:54 -0800

    Finish part 2

--- 2019/10/2.py
+++ 2019/10/2.py
@@ -2,65 +2,35 @@ import itertools
 import math
 import numpy as np
 
-def main():
-    paths = {}
+def get_asteroids():
     asteroids = []
     _map = read()
     for h in range(0, len(_map)):
         for w in range(0, len(_map[h])):
             if _map[h][w] == "#":
                 asteroids.append((w, h))
+    return asteroids
 
-    #print(asteroids)
-    origin = (5,8)
-    _list = [a for a in asteroids if a != origin]
+def get_angle(_from, _to):
     origin = (0,0)
-    #print(_list)
-    #return
-    for point in _list:
-        point = (point[0] - 5, point[1] - 8)
-        #print(str(origin[::1]) + " to " + str(point[::1]))
+    point = (_to[0] - _from[0], _to[1] - _from[1])
+    return round(np.rad2deg((np.arctan2(*origin[::-1]) - np.arctan2(*point[::-1])) % (2 * np.pi)), 2)
 
-        slope = math.degrees(math.atan2((point[1] - origin[1]), (point[0] - origin[0])))
-        ang1 = np.arctan2(*origin[::-1])
-        print(ang1)
-        ang2 = np.arctan2(*point[::-1])
-        print(ang2)            
-        print(str(origin[::1]) + " to " + str(point[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
-        dot = point[0] * origin[0] + point[1] * origin[1]
-        det = point[1] * origin[0] - point[0] * origin[1]
-        angle = math.atan2(det, dot)
-        print("Angle: " + str(np.rad2deg(angle % (2 * np.pi))))
-
-        continue
-
+def get_optimal_origin(asteroids):
+    paths = {}
+    origin = (0,0)
+    for a, b in itertools.combinations(asteroids, 2):
         if not a in paths:
             paths[a] = {}
-        slope = math.degrees(math.atan2((b[1] - a[1]), (b[0] - a[0])))
-        paths[a][slope] = True
 
-        if a == (5,8):
-            ang1 = np.arctan2(*b[::-1])
-            print(ang1)
-            ang2 = np.arctan2(*a[::-1])
-            print(ang2)
-            print(str(b[::1]) + " to " + str(a[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
+        angle = get_angle(a, b)
+        paths[a][angle] = True
 
         if not b in paths:
             paths[b] = {}
-        slope = math.degrees(math.atan2((a[1] - b[1]), (a[0] - b[0])))
-        paths[b][slope] = True
-
-        if b == (5,8):
-            ang1 = np.arctan2(*a[::-1])
-            print(ang1)
-            ang2 = np.arctan2(*b[::-1])
-            print(ang2)            
-            print(str(a[::1]) + " to " + str(b[::1]) + ": " + str(slope) + " - " + str(np.rad2deg((ang1 - ang2) % (2 * np.pi))))
-            dot = b[0] * a[0] + b[1] * a[1]
-            det = b[1] * a[0] - b[0] * a[1]
-            angle = math.atan2(det, dot)
-            print("Angle: " + str(np.rad2deg(angle % (2 * np.pi))))
+        
+        angle = get_angle(b, a)
+        paths[b][angle] = True
     
     _path = None
     _max = 0
@@ -70,6 +40,42 @@ def main():
             _path = path
             
     print(str(_path) + ": " + str(len(paths[_path])))
+    return _path
+
+def get_distance(_from, _to):
+    return abs(_to[0] - _from[0]) + abs(_to[1] - _from[1])
+
+class Asteroid:
+    def __init__(self, point, distance):
+        self.point = point
+        self.distance = distance
+
+def main():
+    asteroids = get_asteroids()
+    origin = get_optimal_origin(asteroids)
+
+    paths = {}
+    _list = [a for a in asteroids if a != origin]
+    for point in _list:
+        angle = get_angle(origin, point)
+        distance = get_distance(origin, point)
+        if not angle in paths:
+            paths[angle] = []
+        paths[angle].append(Asteroid(point[::1], distance))
+    
+    for path in paths:
+        paths[path].sort(key=lambda asteroid: asteroid.distance)
+
+    destroyed = 0
+    asteroid = None
+    while destroyed < 201:
+        for degree in range(36000, -1, -1):
+            degree = round(np.rad2deg((np.deg2rad(degree / 100) + np.deg2rad(90)) % (2 * np.pi)), 2)
+            if degree in paths:
+                asteroid = paths[degree].pop(0)
+                destroyed += 1
+                if destroyed == 200:
+                    print("Destroyed " + str(destroyed) + ": " + str(asteroid.point))
 
 def read():
     input = open("input.txt", "r")
