1cd9df4b29561edca2c9fc97cada7ca29d4c5615
Ryan <ryanlsmith@me.com>
2019-12-18 15:52:47 -0800

    16 part 2

--- 2019/16/2.py
+++ 2019/16/2.py
@@ -1,38 +1,22 @@
 def main():
-    pattern = [0, 1, 0, -1]
-
     data = read()
+
+    final = []
+    for _ in range(10000):
+        final += data
+
     offset = int("".join(str(i) for i in data[:7]))
-    offset = ((len(data) * 10000) - offset) % len(data) - 1
-    print(offset)
 
     for _ in range(100):
         out = []
-        round = 1
-        for _ in data:
-            match = 0
-            compare = round - 1
-            pos = 0
-            idx = 1
-            val = 0
-            for i in data:
-                mult = i * pattern[pos % 4]
-                try:
-                    if idx % compare == match:
-                        pos += 1
-                        compare = round
-                        match = compare - 1
-                except ZeroDivisionError:
-                    pos += 1
-                    mult = i * pattern[pos % 4]
-
-                val += mult
-                idx += 1
-            parts = [*str(abs(val))]
-            out.append(int(parts[len(parts) - 1]))
-            round += 1
-        data = out
-        print("".join(str(i) for i in data[offset:offset + 8]))
+        total = 0
+        for i in range(len(final) - 1, offset - 1, -1):
+            total += final[i]
+            out.append(total % 10)
+        out.reverse()
+        final = [0] * offset
+        final += out
+    print("".join(str(i) for i in out[:8]))
 
 def read():
     _input = open("input.txt", "r")
