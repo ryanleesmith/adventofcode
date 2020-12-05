e3b211b6731adc26fbfaac875f7a0dbe75d07d5f
Ryan Smith <ryanlsmith@me.com>
2020-12-05 10:20:50 -0800

    Day 5

---
+++ 2020/5/1.py
@@ -0,0 +1,20 @@
+import math
+
+def main():
+    input = open("input.txt", "r")
+    print(max(map(lambda seat: check(seat), input.read().splitlines())))
+
+def check(seat):
+    rng = [0, 127]
+    row_num = sum(map(lambda (idx, row): getPos(idx, row == "F", rng), enumerate(seat[:7])))
+    rng = [0, 7]
+    col_num = sum(map(lambda (idx, column): getPos(idx, column == "L", rng), enumerate(seat[-3:])))
+    return int((row_num * 8) + col_num)
+
+def getPos(idx, isFront, rng):
+    split = math.floor((rng[1] - rng[0]) / 2)
+    rng[int(isFront)] = rng[int(not isFront)] + split if isFront else rng[int(not isFront)] - split
+    return rng[0] if (rng[0] == rng[1]) else 0
+
+if __name__ == "__main__":
+    main()
