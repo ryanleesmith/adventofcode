3c7e5158cec0dc645b9cbb7ec9d37fcced335ee5
Ryan Smith <ryanlsmith@me.com>
2020-12-30 09:45:37 -0800

    Day 15

---
+++ 2020/15/2.py
@@ -0,0 +1,27 @@
+def main():
+    starting = [9,19,1,6,0,5,4]
+    spoken = {}
+
+    for turn in range(1, 30000001):
+        if turn <= len(starting):
+            prev = starting[turn-1]
+            spoken[prev] = (turn, 0)
+        else:
+            if prev in spoken:
+                if spoken[prev][1] == 0:
+                    prev = 0
+                else:
+                    prev = spoken[prev][1] - spoken[prev][0]
+
+            if prev in spoken:
+                if spoken[prev][1] == 0:
+                    spoken[prev] = (spoken[prev][0], turn)
+                else:
+                    spoken[prev] = (spoken[prev][1], turn)
+            else:
+                spoken[prev] = (turn, 0)
+
+    print(prev)
+
+if __name__ == "__main__":
+    main()
