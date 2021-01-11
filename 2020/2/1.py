3f54870080cf43b070f2e8a9b8843250dd1019e5
Ryan Smith <ryanlsmith@me.com>
2021-01-11 00:19:54 -0800

    Update Day 2

--- 2020/2/1.py
+++ 2020/2/1.py
@@ -1,25 +1,11 @@
-import re
-
-class Password:
-    def __init__(self, data):
-        self.min = int(data[0])
-        self.max = int(data[1])
-        self.letter = data[2]
-        self.password = data[3]
-
-    def isValid(self):
-        return self.min <= self.password.count(self.letter) and self.password.count(self.letter) <= self.max
-
 def main():
-    print(sum(map(lambda x: x.isValid(), read())))
-
-def read():
-    data = []
     input = open("input.txt", "r")
-    for line in input.read().splitlines():
-        tuples = re.findall("(\d+)-(\d+) ([a-z]): ([a-z]+)", line)
-        data.append([Password(entry) for entry in tuples][0])
-    return data
+    print(sum(map(lambda line: verify(line), input.read().splitlines())))
+
+def verify(line):
+    (scope, letter, password) = map(lambda x: x.rstrip(":"), line.split())
+    (lower, upper) = map(lambda x: int(x), scope.split("-"))
+    return lower <= password.count(letter) <= upper
 
 if __name__ == "__main__":
     main()
